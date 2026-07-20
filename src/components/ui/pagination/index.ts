f4ae30284dc1a9c1fdcb6ca0b457d025c3662eaf
shadazam08 <azamshad08@gmail.com>
2026-07-20 10:27:54 +0530

    Phase 2.2 Batch 3 completed

---
+++ src/components/ui/pagination/index.ts
@@ -0,0 +1,15 @@
+export { default as Pagination } from "./Pagination";
+export { default as PaginationInfo } from "./PaginationInfo";
+export { default as PaginationSummary } from "./PaginationSummary";
+
+export type {
+    PaginationProps,
+} from "./Pagination";
+
+export type {
+    PaginationInfoProps,
+} from "./PaginationInfo";
+
+export type {
+    PaginationSummaryProps,
+} from "./PaginationSummary";
