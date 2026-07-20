823461fb2964230d43e19fbd98b8053455c35142
shadazam08 <azamshad08@gmail.com>
2026-07-20 19:12:56 +0530

    Phase 2.2 Batch 9 completed

---
+++ src/components/ui/avatar/index.ts
@@ -0,0 +1,11 @@
+export { default as Avatar } from "./Avatar";
+export { default as AvatarGroup } from "./AvatarGroup";
+
+export type {
+    AvatarProps,
+} from "./Avatar";
+
+export type {
+    AvatarGroupItem,
+    AvatarGroupProps,
+} from "./AvatarGroup";
