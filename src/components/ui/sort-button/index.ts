4235ca800dc1f929bd7c63cadf21bc1dd18ce2bb
shadazam08 <azamshad08@gmail.com>
2026-07-20 18:42:37 +0530

    Phase 2.2 Batch 6 completed

---
+++ src/components/ui/sort-button/index.ts
@@ -0,0 +1,3 @@
+export { default as SortButton } from "./SortButton";
+
+export type { SortButtonProps, SortDirection } from "./SortButton";
--- src/components/ui/table/index.ts
+++ src/components/ui/table/index.ts
@@ -1,16 +1,10 @@
-export {
-    Table,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableHeaderCell,
-    TableCell,
-} from "./Table";
+export { Table, TableHead, TableBody, TableRow, TableHeaderCell, TableCell } from "./Table";
 
-export type {
-    TableProps,
-    TableSectionProps,
-    TableRowProps,
-    TableHeaderCellProps,
-    TableCellProps,
-} from "./Table";
+export { default as TableSortHeader } from "./TableSortHeader";
+export { default as TableActions } from "./TableActions";
+
+export type { TableProps, TableSectionProps, TableRowProps, TableHeaderCellProps, TableCellProps } from "./Table";
+
+export type { TableSortHeaderProps } from "./TableSortHeader";
+
+export type { TableActionsProps } from "./TableActions";
