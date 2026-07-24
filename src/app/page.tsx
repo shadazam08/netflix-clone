7e914c22267891425def918ee16e60dcbeed4dc0
shadazam08 <azamshad08@gmail.com>
2026-07-24 17:53:17 +0530

    feat: create project foundation

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold tracking-wide text-red-600">
+        Netflix Clone
+      </h1>
+    </main>
+  );
+}
