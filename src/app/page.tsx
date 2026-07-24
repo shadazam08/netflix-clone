b7cd15bf606c51c4e41b28d085d3d351896b6f1e
shadazam08 <azamshad08@gmail.com>
2026-07-24 18:00:09 +0530

    chore: configure prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold tracking-wide text-red-600">
-        Netflix Clone
-      </h1>
+      <h1 className="text-4xl font-bold tracking-wide text-red-600">Netflix Clone</h1>
     </main>
   );
 }
