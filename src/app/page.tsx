9ac819c61d0f887a14a1ce81ea0942cf84122025
shadazam08 <azamshad08@gmail.com>
2026-07-26 21:34:55 +0530

    delete

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold tracking-wide text-red-600">Netflix Clone</h1>
-    </main>
-  );
-}
